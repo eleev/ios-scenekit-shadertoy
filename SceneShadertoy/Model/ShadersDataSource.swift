84d0832341fb11826a31a5702a7bf7f41df30d21
Astemir Eleev <astemireleev@gmail.com>
2017-09-01 19:20:02 +0300

    Implemented ShaderDataSource class

---
+++ SceneShadertoy/Model/ShadersDataSource.swift
@@ -0,0 +1,17 @@
+//
+//  ShadersDataSource.swift
+//  SceneShadertoy
+//
+//  Created by Astemir Eleev on 01/09/2017.
+//  Copyright © 2017 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+/// Shader Data Source defines collections of different shader settings for different use cases. This class is supposed to be fed into some sort of host manager/handler e.g. UITableViewController, UICollectionViewController or something similar.
+class ShadersDataSource {
+    var geometrySettings = Settings(items: [
+        NoneSettingItem(),
+        ShaderSettingItem(shaderData: ShaderData(filename: "geometry_ripple"))
+        ])
+}
