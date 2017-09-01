2e41a4498afc4ae8b5af11d15d5f50ea172f706b
Astemir Eleev <astemireleev@gmail.com>
2017-09-01 16:20:21 +0300

    Created SettingItemType protocol

---
+++ SceneShadertoy/Model/Settings/SettingItemType.swift
@@ -0,0 +1,14 @@
+//
+//  SettingItem.swift
+//  SceneShadertoy
+//
+//  Created by Astemir Eleev on 01/09/2017.
+//  Copyright © 2017 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol SettingItemType {
+    var title: String {get}
+    var selected: Bool {get set}
+}
