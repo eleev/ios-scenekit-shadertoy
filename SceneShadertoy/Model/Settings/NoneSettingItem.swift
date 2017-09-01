3cb574a387d411df28e56256c9f288a94320067f
Astemir Eleev <astemireleev@gmail.com>
2017-09-01 16:20:07 +0300

    Created NoneSettingItem class

---
+++ SceneShadertoy/Model/Settings/NoneSettingItem.swift
@@ -0,0 +1,14 @@
+//
+//  NoneSettingItem.swift
+//  SceneShadertoy
+//
+//  Created by Astemir Eleev on 01/09/2017.
+//  Copyright © 2017 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+class NoneSettingItem: SettingItemType {
+    var title: String = "None"
+    var selected: Bool = true
+}
